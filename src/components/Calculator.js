744486d75ced1adf4877817ec57439e0b3022f30
katarighe <m.ighe@hotmail.com>
2023-08-08 12:17:13 +0300

    Add button type

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
 
 export default class Calculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    
-    render() {
-        return (
-        <div>
-            <div className="screen">
-              <span>0</span>
-            </div>
-            <div className="buttons">
-              <button>7</button>
-              <button>8</button>
-              <button>9</button>
-              <button>+</button>
-            </div>
-            <div className="buttons">
-              <button>4</button>
-              <button>5</button>
-              <button>6</button>
-              <button>-</button>
-            </div>
-            <div className="buttons">
-              <button>1</button>
-              <button>2</button>
-              <button>3</button>
-              <button>*</button>
-            </div>
-            <div className="buttons">
-              <button>0</button>
-              <button>.</button>
-              <button>C</button>
-              <button>/</button>
-            </div>
-          </div>
-        );
-      };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="screen">
+          <span>0</span>
+        </div>
+        <div className="buttons">
+          <button type="button" value="7">7</button>
+          <button type="button" value="8">8</button>
+          <button type="button" value="9">9</button>
+          <button type="button" value="+">+</button>
+        </div>
+        <div className="buttons">
+          <button type="button" value="4">4</button>
+          <button type="button" value="5">5</button>
+          <button type="button" value="6">6</button>
+          <button type="button" value="-">-</button>
+        </div>
+        <div className="buttons">
+          <button type="button" value="1">1</button>
+          <button type="button" value="2">2</button>
+          <button type="button" value="3">3</button>
+          <button type="button" value="*">*</button>
+        </div>
+        <div className="buttons">
+          <button type="button" value="0">0</button>
+          <button type="button" value=".">.</button>
+          <button type="button" value="C">C</button>
+          <button type="button" value="/">/</button>
+        </div>
+      </div>
+    );
+  }
 }
