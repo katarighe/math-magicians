213a891056d2cf17e634e03ccf08a769e429f413
katarighe <m.ighe@hotmail.com>
2023-08-08 19:17:26 +0300

    Add the onclick function inside the buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,24 +1,24 @@
 const Buttons = () => (
   <div className="calculator_UI">
-    <button type="button" value="clear">AC</button>
-    <button type="button" value="pos-neg">+/-</button>
-    <button type="button" value="%">%</button>
-    <button type="button" value="/" className="btn_opaque">÷</button>
-    <button type="button" value="7">7</button>
-    <button type="button" value="8">8</button>
-    <button type="button" value="9">9</button>
-    <button type="button" value="*" className="btn_opaque">X</button>
-    <button type="button" value="4">4</button>
-    <button type="button" value="5">5</button>
-    <button type="button" value="6">6</button>
-    <button type="button" value="-" className="btn_opaque">-</button>
-    <button type="button" value="1">1</button>
-    <button type="button" value="2">2</button>
-    <button type="button" value="3">3</button>
-    <button type="button" value="+" className="btn_opaque">+</button>
-    <button type="button" value="0" className="btn_large">0</button>
-    <button type="button" value=".">.</button>
-    <button type="button" value="=" className="btn_opaque">=</button>
+    <button type="button" onClick={this.buttonClicked} value="clear">AC</button>
+    <button type="button" onClick={this.buttonClicked} value="pos-neg">+/-</button>
+    <button type="button" onClick={this.buttonClicked} value="%">%</button>
+    <button type="button" onClick={this.buttonClicked} value="/" className="btn_opaque">÷</button>
+    <button type="button" onClick={this.buttonClicked} value="7">7</button>
+    <button type="button" onClick={this.buttonClicked} value="8">8</button>
+    <button type="button" onClick={this.buttonClicked} value="9">9</button>
+    <button type="button" onClick={this.buttonClicked} value="*" className="btn_opaque">X</button>
+    <button type="button" onClick={this.buttonClicked} value="4">4</button>
+    <button type="button" onClick={this.buttonClicked} value="5">5</button>
+    <button type="button" onClick={this.buttonClicked} value="6">6</button>
+    <button type="button" onClick={this.buttonClicked} value="-" className="btn_opaque">-</button>
+    <button type="button" onClick={this.buttonClicked} value="1">1</button>
+    <button type="button" onClick={this.buttonClicked} value="2">2</button>
+    <button type="button" onClick={this.buttonClicked} value="3">3</button>
+    <button type="button" onClick={this.buttonClicked} value="+" className="btn_opaque">+</button>
+    <button type="button" onClick={this.buttonClicked} value="0" className="btn_large">0</button>
+    <button type="button" onClick={this.buttonClicked} value=".">.</button>
+    <button type="button" onClick={this.buttonClicked} value="=" className="btn_opaque">=</button>
   </div>
 );
 
