7f9aa8c299e0cf9a84cd43ca329e220ccbb23063
katarighe <m.ighe@hotmail.com>
2023-08-08 12:59:56 +0300

    Rearrange the calculator format, including num

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,39 +8,32 @@ export default class Calculator extends Component {
 
   render() {
     return (
-      <div>
-        <div className="screen">
-          <span>0</span>
-        </div>
-        <div className="buttons">
-          <button type="button" value="clear">AC</button>
-          <button type="button" value="pos-neg">+/-</button>
-          <button type="button" value="%">%</button>
-          <button type="button" value="/">÷</button>
-          <button type="button" value="7">7</button>
-          <button type="button" value="8">8</button>
-          <button type="button" value="9">9</button>
-          <button type="button" value="+">+</button>
-        </div>
-        <div className="buttons">
-          <button type="button" value="4">4</button>
-          <button type="button" value="5">5</button>
-          <button type="button" value="6">6</button>
-          <button type="button" value="-">-</button>
-        </div>
-        <div className="buttons">
-          <button type="button" value="1">1</button>
-          <button type="button" value="2">2</button>
-          <button type="button" value="3">3</button>
-          <button type="button" value="*">*</button>
-        </div>
-        <div className="buttons">
-          <button type="button" value="0">0</button>
-          <button type="button" value=".">.</button>
-          <button type="button" value="C">C</button>
-          <button type="button" value="/">/</button>
-        </div>
+      <div className="calculator">
+        <div className="calculator_container">
+          <div className="calculator_screen">0</div>
+          <div className="calculatorUI">
+            <button type="button" value="clear">AC</button>
+            <button type="button" value="pos-neg">+/-</button>
+            <button type="button" value="%">%</button>
+            <button type="button" value="/">÷</button>
+            <button type="button" value="7">7</button>
+            <button type="button" value="8">8</button>
+            <button type="button" value="9">9</button>
+            <button type="button" value="*">*</button>
+            <button type="button" value="4">4</button>
+            <button type="button" value="5">5</button>
+            <button type="button" value="6">6</button>
+            <button type="button" value="-">-</button>
+            <button type="button" value="1">1</button>
+            <button type="button" value="2">2</button>
+            <button type="button" value="3">3</button>
+            <button type="button" value="+">+</button>
+            <button type="button" value="0">0</button>
+            <button type="button" value=".">.</button>
+            <button type="button" value="=">/</button>
+         </div>
       </div>
+  </div>
     );
   }
 }
