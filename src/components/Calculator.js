bb49f9ef3f31ae20db31bcd40a15569d4b77b08c
katarighe <m.ighe@hotmail.com>
2023-08-08 11:58:13 +0300

    Create numbers and buttons of the calcuator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,42 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 
 export default class Calculator extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
+    
+    render() {
+        return (
+        <div>
+            <div className="screen">
+              <span>0</span>
+            </div>
+            <div className="buttons">
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>+</button>
+            </div>
+            <div className="buttons">
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>-</button>
+            </div>
+            <div className="buttons">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>*</button>
+            </div>
+            <div className="buttons">
+              <button>0</button>
+              <button>.</button>
+              <button>C</button>
+              <button>/</button>
+            </div>
+          </div>
+        );
+      };
 }
