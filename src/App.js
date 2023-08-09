dd17c16018e5671618386174e78a1e6fa51cd0a2
katarighe <m.ighe@hotmail.com>
2023-08-09 18:49:58 +0300

    Fix line error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  renderQuote() {
+    return (
+      <Quote />
+    );
   }
 
   render() {
