8c467652d91e03ef24bcbac9c4ab26a914d58723
katarighe <m.ighe@hotmail.com>
2023-08-09 18:41:54 +0300

    Add export default function

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,1 +1,7 @@
 import { useEffect, useState } from 'react';
+
+export default function Quote() {
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState('');
+}
