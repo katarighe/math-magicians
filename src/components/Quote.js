c64c09e108af3977df45467519897b6eaee7adb5
katarighe <m.ighe@hotmail.com>
2023-08-09 19:15:21 +0300

    Format the line, and fix bugs relating to API

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -7,33 +7,33 @@ export default function Quote() {
 
   useEffect(() => {
     setLoading(true);
-        const fetchQuote = async () => {
-            try {
-                const res = await fetch ('https://api.api-ninjas.com/v1/quotes?category=intelligence', {
-                    method: 'GET',
-                    withCredientials: true,
-                    headers: {
-                        'X-Api-Key': 'JPH0dr8vp5cPyFyCj/jr7g==7Z5eQyE7zjkxZ4gW',
-                        'Content-Type': 'application/json',
-                    },
-                });
-                const json = await res.json();
-                setData(json[0]);
-            } catch (error) {
-                setError (true);
-            }
-            setLoading (false);
-        };
-        fetchQuote();
-    }, [setData, setLoading, setError]);
+    const fetchQuote = async () => {
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=intelligence', {
+          method: 'GET',
+          withCredientials: true,
+          headers: {
+            'X-Api-Key': 'JPH0dr8vp5cPyFyCj/jr7g==7Z5eQyE7zjkxZ4gW',
+            'Content-Type': 'application/json',
+          },
+        });
+        const json = await res.json();
+        setData(json[0]);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchQuote();
+  }, [setData, setLoading, setError]);
 
-    if (error) return <div className="error_message">Something went wrong! Please try again!</div>;
-    if (loading) return <div className="loading_message">Loading... Please wait.</div>;
+  if (error) return <div className="error_message">Something went wrong! Please try again!</div>;
+  if (loading) return <div className="loading_message">Loading... Please wait.</div>;
 
-    return (
-        <div className="quote_container">
+  return (
+    <div className="quote_container">
       <p className="quote">{data.quote}</p>
       <p className="author">{data.author}</p>
-      </div>
+    </div>
   );
 }
