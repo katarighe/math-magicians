aa6c8649a648616e571f3a8d38f357bffaf3f53c
katarighe <m.ighe@hotmail.com>
2023-08-15 14:32:27 +0300

    Add a header below the quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -5,24 +5,27 @@ import './Quote.css';
 const Quote = () => {
   <Header />;
   return (
-    <div className="quote">
-      <p className="description">
-        Mathematics is the language of science.
-        It is the language of logic and reason.
-        It is the language of the universe.
-        Without mathematics, we would not be able
-        to understand the world around us. We would
-        not be able to make predictions about the future.
-        We would not be able to create new technologies.
-        Mathematics is essential to our understanding
-        of the world and our ability to shape it.
-      </p>
-      <big>
-        <b className="author">
-          Paul Erdős, a Hungarian mathematician known to be
-          one of the greatest mathematicians of the 20th century
-        </b>
-      </big>
+    <div className="calculator_header">
+      <h1 className="header">Read this favorite quote on math!</h1>
+      <div className="quote">
+        <p className="description">
+          Mathematics is the language of science.
+          It is the language of logic and reason.
+          It is the language of the universe.
+          Without mathematics, we would not be able
+          to understand the world around us. We would
+          not be able to make predictions about the future.
+          We would not be able to create new technologies.
+          Mathematics is essential to our understanding
+          of the world and our ability to shape it.
+        </p>
+        <big>
+          <b className="author">
+            Paul Erdős, a Hungarian mathematician known to be
+            one of the greatest mathematicians of the 20th century
+          </b>
+        </big>
+      </div>
     </div>
   );
 };
