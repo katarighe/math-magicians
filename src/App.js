8c4550488f4e99001ea366a3b56571dc0a972a94
katarighe <m.ighe@hotmail.com>
2023-08-15 10:38:04 +0300

    Change the layout and convert into full website.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
-function App() {
-  return (
-    <>
-      <Calculator />
-      <Quote />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = [];
+  }
+
+  render() {
+    return (
+      <div>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calculator" element={(<Calculator />)} />
+            <Route path="/Quote" element={<Quote />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
