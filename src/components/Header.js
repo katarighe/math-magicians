cd2452d314018a30061b8d54dfa5bb24ab533a60
katarighe <m.ighe@hotmail.com>
2023-08-15 10:51:09 +0300

    Create a header for the website

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header';
+
+const Header = () (
+    <div>
+        <h1 className="title">Welcome to Math Magicians!</h1>
+    </div>
+);
+
+export default Header;
