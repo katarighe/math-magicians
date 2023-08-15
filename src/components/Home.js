fe27a6b61b21e19e67d7532dc19dfa719504eb71
katarighe <m.ighe@hotmail.com>
2023-08-15 13:16:51 +0300

    Make major changes on the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,20 +6,27 @@ const Home = () => (
     <div className="home_page">
       <h2 className="home_heading">Welcome to Math Magicians!</h2>
       <p className="home_paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tellus
-        augue, tincidunt malesuada nulla sed, rutrum porttitor dui. Nunc egestas
-        lectus vitae ante aliquet lobortis. Integer nec elementum augue. Morbi
-        imperdiet gravida elit id pulvinar. Fusce scelerisque eleifend pretium.
-        In ultrices mi nec laciniaslacinia. Nulla facilisi. In vestibulum nulla
-        sed tempor vehicula.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus metus
+        augue, mattis vel iaculis non, fringilla vitae orci. Cras eu lectus non
+        tellus ullamcorper consectetur id ac odio. Phasellus convallis a lacus
+        eget semper. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Aenean vestibulum nibh ac dolor malesuada varius. Morbi nec posuere magna.
+        Morbi bibendum eros vel ligula tempus, sit amet cursus libero aliquam. Duis
+        vestibulum eu quam vel lacinia. Pellentesque vel mollis nisl, in feugiat risus.
+        Aliquam erat volutpat. Suspendisse id ullamcorper lectus, eget consequat elit.
       </p>
       <p className="home_paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tellus
-        augue, tincidunt malesuada nulla sed, rutrum porttitor dui. Nunc egestas
-        lectus vitae ante aliquet lobortis. Integer nec elementum augue. Morbi
-        imperdiet gravida elit id pulvinar. Fusce scelerisque eleifend pretium.
-        In ultrices mi nec laciniaslacinia. Nulla facilisi. In vestibulum nulla
-        sed tempor vehicula.
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+        himenaeos. Etiam vel erat dui. Mauris quam felis, vulputate ut commodo at, fringilla
+        nec felis. Quisque tempor et purus nec suscipit. Morbi porta enim in odio tempor viverra.
+        Integer pretium, dui sit amet bibendum pharetra, ipsum mi vehicula enim, in pulvinar purus
+        eros non nulla. Quisque efficitur volutpat lorem, sed vestibulum tellus hendrerit nec.
+        Pellentesque et venenatis purus. Nulla malesuada, lectus a tincidunt vehicula, neque odio
+        sollicitudin lorem, nec consectetur felis ex at velit. Nullam commodo mi quis tellus
+        tempus lobortis. Vestibulum cursus justo neque, non pharetra urna malesuada ac. Duis
+        bibendum augue in magna consequat vestibulum. Mauris sit amet elit tortor. Sed
+        egestas blandit magna eu rhoncus. Quisque iaculis neque vel enim feugiat, vel
+        lobortis arcu mattis. Curabitur quis enim ipsum.
       </p>
     </div>
   </div>
