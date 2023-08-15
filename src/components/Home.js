ab3ca7ffd6e7737fc9cab8b5d863c008cc9994a2
katarighe <m.ighe@hotmail.com>
2023-08-15 11:38:26 +0300

    Create a homepage for Math Magicians website

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div>
+    <div className="home_page">
+      <h2 className="home_heading">Welcome to Math Magicians!</h2>
+      <p className="home_paragraph">
+        The Math Magicians application is a great way for students to showcase
+        their math skills and creativity. The application is open to students
+        of all ages and skill levels, and there are no prerequisites.
+        Students can submit their applications in a variety of ways,
+        including writing essays, creating videos, or making presentations.
+      </p>
+      <p className="home_paragraph">
+        The Math Magicians application is a great way for students to learn about
+        different mathematical concepts and to develop their problem-solving skills.
+        Students who apply to the program will have the opportunity to work with other
+        students who are passionate about math, and they will receive feedback
+        from mentors who are experts in the field.
+      </p>
+      <p className="home_paragraph">
+        In addition to learning about math, students who participate in the Math Magicians
+        program will also have the opportunity to compete in a variety of math competitions.
+        These competitions are a great way for students to test their skills
+        and to see how they stack up against other students from around the world.
+        If you are a student who loves math and who is looking for a challenge,
+        then the Math Magicians application is for you.
+        The program is a great way to learn about math, to develop your problem-solving skills,
+        and to compete against other students from around the world.
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
