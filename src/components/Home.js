9fb3d841826f1c7154c52e1738df9ec75e1e67bc
katarighe <m.ighe@hotmail.com>
2023-08-15 12:35:09 +0300

    Change to "Lorem ipsum dolor sit amet" instead, to match exactly with the requirements.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,28 +6,20 @@ const Home = () => (
     <div className="home_page">
       <h2 className="home_heading">Welcome to Math Magicians!</h2>
       <p className="home_paragraph">
-        The Math Magicians application is a great way for students to showcase
-        their math skills and creativity. The application is open to students
-        of all ages and skill levels, and there are no prerequisites.
-        Students can submit their applications in a variety of ways,
-        including writing essays, creating videos, or making presentations.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tellus
+        augue, tincidunt malesuada nulla sed, rutrum porttitor dui. Nunc egestas
+        lectus vitae ante aliquet lobortis. Integer nec elementum augue. Morbi
+        imperdiet gravida elit id pulvinar. Fusce scelerisque eleifend pretium.
+        In ultrices mi nec laciniaslacinia. Nulla facilisi. In vestibulum nulla
+        sed tempor vehicula.
       </p>
       <p className="home_paragraph">
-        The Math Magicians application is a great way for students to learn about
-        different mathematical concepts and to develop their problem-solving skills.
-        Students who apply to the program will have the opportunity to work with other
-        students who are passionate about math, and they will receive feedback
-        from mentors who are experts in the field.
-      </p>
-      <p className="home_paragraph">
-        In addition to learning about math, students who participate in the Math Magicians
-        program will also have the opportunity to compete in a variety of math competitions.
-        These competitions are a great way for students to test their skills
-        and to see how they stack up against other students from around the world.
-        If you are a student who loves math and who is looking for a challenge,
-        then the Math Magicians application is for you.
-        The program is a great way to learn about math, to develop your problem-solving skills,
-        and to compete against other students from around the world.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tellus
+        augue, tincidunt malesuada nulla sed, rutrum porttitor dui. Nunc egestas
+        lectus vitae ante aliquet lobortis. Integer nec elementum augue. Morbi
+        imperdiet gravida elit id pulvinar. Fusce scelerisque eleifend pretium.
+        In ultrices mi nec laciniaslacinia. Nulla facilisi. In vestibulum nulla
+        sed tempor vehicula.
       </p>
     </div>
   </div>
