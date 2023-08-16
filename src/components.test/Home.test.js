37fdb1ec24c11268fec73589cd2b0471109644ba
GhostEsso <lilipitaham@gmail.com>
2023-08-16 10:25:58 +0000

    installation of Jest environment

--- src/components.test/Home.test.js
+++ src/components.test/Home.test.js
@@ -2,3 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Home from '../components/Home';
+
+describe("Home Component", () => {
+  test("renders correctly", () => {
+    const component = renderer.create(<Home />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
