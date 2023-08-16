9249b26157c409cb4ac41bb112957ac6808ad37d
katarighe <m.ighe@hotmail.com>
2023-08-16 13:49:40 +0300

    Fix line and ident errors

--- src/components.test/Calculator.test.js
+++ src/components.test/Calculator.test.js
@@ -4,19 +4,19 @@ import renderer from 'react-test-renderer';
 import Calculator from '../components/Calculator';
 
 describe('Calculator', () => {
-    it('renders the Calculator component', () => {
-        render(<Calculator />);
-    });
+  it('renders the Calculator component', () => {
+    render(<Calculator />);
+  });
 });
 
 it('renders correctly if there is no items present', () => {
-    const tree = renderer.create(<Calculator />).toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 it('checks the UI changes state after the button is clicked', () => {
-    const { getByText } = render(<Calculator />);
-    const number1 = getByText(1);
-    const answer = number1.textContent;
-    expect(answer).toBe('1');
+  const { getByText } = render(<Calculator />);
+  const number1 = getByText(1);
+  const answer = number1.textContent;
+  expect(answer).toBe('1');
 });
