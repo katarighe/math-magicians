4087c6b3723587f95fa1a62fe7cf1d17a393ce9a
katarighe <m.ighe@hotmail.com>
2023-08-16 11:51:53 +0300

    Add the test for the operate function

---
+++ src/logic.tests/operate.test.js
@@ -0,0 +1,28 @@
+import calculate from '../logic/calculate';
+
+const calculateObj = {
+    total: 0,
+    next: null,
+    operation: null,
+}
+
+describe('testing function:', () => {
+    test('Check if the AC button clears the UI to zero (0)', () => {
+        expect(calculate(calculateObj, 'AC')).toEqual(calculateObj);
+    });
+    test('= button along with the operation for performing the operation', () => {
+        const calculateObjct = { total: 4, next: 9, operation: 'x' };
+        const result = { operation: null, next: null, total: '36' };
+        expect(calculate(calculateObjct, '=')).toEqual(result);
+    });    test('the + button', () => {
+        const calculateObjct = { total: 0, next: .9, operation: '+' };
+        expect(calculate(calculateObjct, '.')).toEqual({...calculateObjct, next: '.9' });
+    });
+});
+
+describe('Test of the operation changes if there is already an operation', () => {
+    test('if obj.next is missing', () => {
+        const obj = {...calculateObj, total: '50', operation: '-'};
+        expect(calculate(obj, '+')).toEqual({ ...obj, operation: '+' });
+    });
+});
