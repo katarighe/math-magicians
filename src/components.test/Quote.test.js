56a5ee1ab5e47dda4abb9aa0757335e3f05adc2e
GhostEsso <lilipitaham@gmail.com>
2023-08-16 10:36:11 +0000

    Quote test written

--- src/components.test/Quote.test.js
+++ src/components.test/Quote.test.js
@@ -1,4 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-import Quote from '../components/Quote';
+import React from "react";
+import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import Quote from "../components/Quote";
+
+test("Snapshot test for Quote component", () => {
+  const component = renderer.create(<Quote />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
