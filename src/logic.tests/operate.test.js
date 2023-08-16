6befbf71a8a2132b945e09746a5f90c782a207a0
katarighe <m.ighe@hotmail.com>
2023-08-16 13:06:31 +0300

    Fix syntax on operate

--- src/logic.tests/operate.test.js
+++ src/logic.tests/operate.test.js
@@ -6,23 +6,23 @@ const calculateObj = {
   operation: null,
 };
 
-describe('testing function:', () => {
+describe('Test the function:', () => {
   test('Check if the AC button clears the UI to zero (0)', () => {
     expect(calculate(calculateObj, 'AC')).toEqual(calculateObj);
   });
-  test('= button along with the operation for performing the operation', () => {
+  test('The = button along with the operation for performing the operation', () => {
     const calculateObjct = { total: 4, next: 9, operation: 'x' };
     const result = { operation: null, next: null, total: '36' };
     expect(calculate(calculateObjct, '=')).toEqual(result);
   });
-  test('the . button', () => {
+  test('The . button', () => {
     const calculateObjct = { total: '0', next: '.5', operation: '+' };
     expect(calculate(calculateObjct, '.')).toEqual({ ...calculateObjct, next: '.5' });
   });
 });
 
 describe('Test of the operation changes if there is already an operation', () => {
-  test('if obj.next is missing', () => {
+  test('In case if the obj.next is missing', () => {
     const obj = { ...calculateObj, total: '50', operation: '-' };
     expect(calculate(obj, '+')).toEqual({ ...obj, operation: '+' });
   });
